ad5a14e57d45cf555707273b8cf9f580d786e33f
balen001 <113919528+balen001@users.noreply.github.com>
2024-08-11 14:27:58 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -5,13 +5,7 @@ function redirectToOSSpecificPage() {
         return window.location.href = 'https://play.google.com/store/apps/details?id=com.nasswallet.customer&hl=en';
     } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
         return window.location.href = 'https://apps.apple.com/iq/app/nasswallet/id1511124150';
-    } else if (/Windows NT/.test(userAgent)) {
-        return window.location.href = 'https://nass.iq/';
-    } else if (/Macintosh/.test(userAgent)) {
-        return window.location.href = 'https://nass.iq/';
-    } else if (/Linux/.test(userAgent)) {
-        return window.location.href = 'https://nass.iq/';
-    } else {
+    }  else {
         return window.location.href = 'https://nass.iq/';
     }
 }
